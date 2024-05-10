d7eb247a62728fc6473ef1ce556bb26ce8f1eaba
Tahmina979 <55082977+Tahmina979@users.noreply.github.com>
2024-05-10 14:45:05 +0800

    Add files via upload

---
+++ visualize_skeleton_point_cloud.m
@@ -0,0 +1,28 @@
+clc
+clear
+clear all;
+
+% #### DIRECTORY OF THE DATASET #####
+
+path_to_swc="address of the directory of .swc files in dataset folder"
+path=dir(strcat(path_to_swc,"*.swc"));
+
+for i=1:length(path)
+    file = fopen(strcat(path_to_swc,path(i).name));
+    data = zeros(0,3);
+    while(~feof(file))
+    fline = fgetl(file);  
+    if fline(1) == '#'
+        continue;
+    end    
+    A = sscanf(fline, '%f %f %f %f %f %f %f');
+    A1=A(3:5);
+    data = [data; A1']
+    end
+    fclose(file);
+    newObj=pointCloud(data); 
+    figure(1);
+    subplot(1, length(path), i);
+    pcshow(newObj);
+end
+return;
